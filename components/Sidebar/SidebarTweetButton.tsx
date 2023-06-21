94d47903823979d0010a4946bcdf7c1ba6dc786b
itexpert120 <itexpert120@gmail.com>
2023-06-21 17:32:37 +0500

    Finish Auth UI

--- components/Sidebar/SidebarTweetButton.tsx
+++ components/Sidebar/SidebarTweetButton.tsx
@@ -2,11 +2,19 @@
 
 import { FaFeather } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import { useCallback } from "react";
+import useLoginModal from "@/hooks/useLoginModal";
 
 export default function SidebarTweetButton() {
   const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   return (
-    <div onClick={() => router.push("/")}>
+    <div onClick={onClick}>
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <FaFeather size={24} color="white" />
       </div>
