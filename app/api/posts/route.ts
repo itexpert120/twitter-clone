cfc647ab0af575670ddd2f638c5270fad0ab9d36
itexpert120 <itexpert120@gmail.com>
2023-06-22 15:34:01 +0500

    Create posts API

---
+++ app/api/posts/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/lib/prismadb";
+
+export async function GET(req: Request) {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        user: true,
+        comments: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return NextResponse.json(posts);
+  } catch (error: any) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ status: "fail", message: error.message }),
+      { status: 400 }
+    );
+  }
+}
