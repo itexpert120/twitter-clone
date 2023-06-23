2b50a8cbc5896a3bb5e56d7fb237ef40b59a1944
itexpert120 <itexpert120@gmail.com>
2023-06-23 22:13:36 +0500

    revert fix

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -4,32 +4,20 @@ import usePosts from "@/hooks/usePosts";
 
 import PostItem from "@/components/post/PostItem";
 import Loading from "@/app/(site)/loading";
-import { useEffect, useState } from "react";
 
 interface PostFeedProps {
   userId?: string;
 }
 
 export default function PostFeed({ userId }: PostFeedProps) {
-  const [posts, setPosts] = useState<Record<string, any>[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("/api/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        setPosts(data);
-        setIsLoading(false);
-      });
-  }, []);
+  const { data: posts = [], isLoading } = usePosts(userId);
 
   if (isLoading) {
     return <Loading />;
   }
   return (
     <>
-      {posts?.map((post: Record<string, any>) => (
+      {posts.map((post: Record<string, any>) => (
         <PostItem userId={userId} key={post.id} data={post} />
       ))}
     </>
