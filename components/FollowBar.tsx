b7b3041203d49a5eed1a6e470cdaaa72154a461c
itexpert120 <itexpert120@gmail.com>
2023-06-24 18:55:11 +0500

    fix sticky who to follow

--- components/FollowBar.tsx
+++ components/FollowBar.tsx
@@ -37,9 +37,9 @@ export default function FollowBar() {
 
   return (
     // Container
-    <div className="px-6 py-4 hidden lg:block sticky top-0">
+    <div className="px-6 py-4 hidden lg:block">
       {/* Background */}
-      <div className="bg-neutral-800 rounded-xl p-4">
+      <div className="bg-neutral-800 rounded-xl p-4 sticky top-0">
         {/* Label */}
         <h2 className="text-white text-xl font-semibold">Who to Follow</h2>
         {/* Users List */}
